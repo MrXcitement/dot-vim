7b1e7def7f8874fdfada57f69454332f8f496e6e
Mike Barker <mike@thebarkers.com>
2015-05-01 10:37:22 -0600

    rename makelinks.sh to install.sh and now force create symlinks

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-# makelinks -- make symbolic links in the users home directory
+# install -- make symbolic links in the users home directory
 # Mike Barker <mike@thebarkers.com>
 # May 14th, 2013
+
+# 2015.04.30 MRB
+# Renamed from makelinks.sh to install.sh
+# Force create the sympolic links
 
 echo "$(basename $0)"
 echo "Make sympolic links in the users home directory to the bash config files and directories"
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 FSPEC="vim*"
-for FILE in ${FSPEC}; do 
+for FILE in ${FSPEC}; do
     echo "Processing file ${FILE}"
     if [ -a ~/.${FILE} ]; then
-		if ! [ -h ~/.${FILE} ]; then
-			mv ~/.${FILE} ~/.${FILE}.backup
-		fi
+	if ! [ -h ~/.${FILE} ]; then
+	    mv ~/.${FILE} ~/.${FILE}.backup
 	fi
-    if ! [ -h ~/.${FILE} ]; then
-		ln -sv ${DIR}/${FILE} ~/.${FILE}
     fi
+    ln -sv ${DIR}/${FILE} ~/.${FILE}
 done
