f23d2e0a71f6dc4a8bab0d29b7fa155ebc9498a7
Mike Barker <mike@thebarkers.com>
2016-04-20 21:32:04 -0600

    Added windows install script

---
+++ install.cmd
@@ -0,0 +1,23 @@
+@echo off
+rem install.cmd -- Install the vim configuration files into the user's profile
+
+rem Mike Barker <mike@thebarkers.com>
+rem April, 20th 2016
+rem Copyright (c) 2016 by Mike Barker
+
+rem Change log:
+rem 2016.04.20
+rem * First release.
+
+rem Check if the current command prompt is running elevated
+whoami /groups | find "12288" >NUL
+if %errorlevel% NEQ 0 (
+	echo You must run the install from an elevated command prompt.
+	exit /B
+)
+
+rem Link the vimrc file and vim directory from the current directory 
+rem to the users profile directory.
+mklink %HOME%\_vimrc %~dp0vimrc
+mklink /d %HOME%\vimfiles %~dp0vim
+
